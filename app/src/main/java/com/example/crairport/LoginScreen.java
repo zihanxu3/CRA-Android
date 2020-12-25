f9f5bbdc75f2e737622f1effb2bdd2673dd2a7e0
jmahajan117 <jay.mahajan33@gmail.com>
2020-12-24 22:14:03 -0500

    Added recording ability for speaking practice.

--- app/src/main/java/com/example/crairport/LoginScreen.java
+++ app/src/main/java/com/example/crairport/LoginScreen.java
@@ -2,9 +2,13 @@ package com.example.crairport;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -24,6 +28,19 @@ public class LoginScreen extends AppCompatActivity {
         setContentView(R.layout.login_screen);
         AuthUser = FirebaseAuth.getInstance();
         setLoginDialog();
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO}, 0);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
     }
 
     /**
--- app/src/main/java/com/example/crairport/SpeakingTest.java
+++ app/src/main/java/com/example/crairport/SpeakingTest.java
@@ -1,15 +1,23 @@
 package com.example.crairport;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -22,10 +30,15 @@ public class SpeakingTest extends AppCompatActivity {
     private int index;
     private ArrayList<String> allText;
     private MediaPlayer mediaPlayer;
-    @Override
+    private ImageView recordButton;
+    private MediaRecorder recorder;
+    private File outputFile;
+    private boolean isRecording = false;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speaking_test);
+        recordButton = (ImageView) findViewById(R.id.recordButton);
 
         // Read file
         Intent current = getIntent();
@@ -50,6 +63,9 @@ public class SpeakingTest extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //Record Sound
+        outputFile = new File( "voiceRecording.mp3");
     }
 
     public void onClickAudio(View view) {
@@ -69,6 +85,7 @@ public class SpeakingTest extends AppCompatActivity {
             String audioFile = allPossible.get(index);
             int resId = getResources().getIdentifier(audioFile, "raw", getPackageName());
             mediaPlayer = MediaPlayer.create(this, resId);
+            mediaPlayer.setVolume(1, 1);
             mediaPlayer.start();
 
         } catch (IOException e) {
@@ -80,5 +97,35 @@ public class SpeakingTest extends AppCompatActivity {
         TextView textView = findViewById(R.id.promptText);
         this.index += 1;
         textView.setText(this.allText.get((index) % (this.allText.size())));
+    }
+
+    public void onRecording(View view) {
+
+        //This works but this needs to be tested on an actual device
+        if (!isRecording) {
+            try {
+                recorder = new MediaRecorder();
+                recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                recorder.setAudioEncoder(MediaRecorder.OutputFormat.AAC_ADTS);
+                recorder.setOutputFile(outputFile);
+
+
+                recorder.prepare();
+                recorder.start();
+                Toast.makeText(getApplicationContext(), "Recording started",
+                        Toast.LENGTH_LONG).show();
+                isRecording = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            Toast.makeText(getApplicationContext(), "Audio Recorder successfully",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
